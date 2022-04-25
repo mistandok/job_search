b77a47bd955912396230cb325f3eea9bc0947e5c
artikov.ak <arti-anton@yandex.ru>
2022-04-25 22:02:47 +0300

    создал адимнку и суперюзера

--- apps/jobsearch/admin.py
+++ apps/jobsearch/admin.py
@@ -1,3 +1,37 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Company, Specialty, Vacancy
+
+
+@admin.register(Company)
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'name',
+        'location',
+        'logo',
+        'description',
+        'employee_count',
+    )
+    search_fields = ('name',)
+
+
+@admin.register(Specialty)
+class SpecialtyAdmin(admin.ModelAdmin):
+    list_display = ('id', 'code', 'title', 'picture')
+
+
+@admin.register(Vacancy)
+class VacancyAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'title',
+        'specialty',
+        'company',
+        'skills',
+        'description',
+        'salary_min',
+        'salary_max',
+        'published_at',
+    )
+    list_filter = ('specialty', 'company', 'published_at')
